eee847c736bacc1784749df7291eed0639ac6fde
Joe Mayinzi <joemayinzi@gmail.com>
2023-09-07 15:33:07 +0100

    fix singleblog page detail

--- src/components/pages/blog/SinglePost.jsx
+++ src/components/pages/blog/SinglePost.jsx
@@ -6,7 +6,7 @@ const SinglePost = () => {
   return (
     <section>
       <div className="container mx-auto mt-20 w-full md:w-[804px] ">
-        <h2 className=" text-4xl font-semibold mt-5 mb-5">
+        <h2 className=" font-semibold mt-5 mb-5 text-3xl">
           The Impact of Technology on the Workplace: How Technology is Changing
         </h2>
         <div className="flex items-center gap-3 mb-9">
@@ -19,42 +19,42 @@ const SinglePost = () => {
           className="w-full mb-8"
           alt="single blog img"
         />
-        <p className=" mb-8 text-xl">
+        <p className=" mb-8 text-xl text-[#3B3C4A] ">
           One of the most rewarding aspects of traveling is immersing yourself
           in the local culture and customs. This includes trying local cuisine,
           attending cultural events and festivals, and interacting with locals.
           Learning a few phrases in the local language can also go a long way in
           making connections and showing respect.
         </p>
-        <p className=" mb-8 text-xl">
+        <p className=" mb-8 text-xl text-[#3B3C4A]">
           One of the most rewarding aspects of traveling is immersing yourself
           in the local culture and customs. This includes trying local cuisine,
           attending cultural events and festivals, and interacting with locals.
           Learning a few phrases in the local language can also go a long way in
           making connections and showing respect.
         </p>
-        <p className=" mb-8 text-xl">
+        <p className=" mb-8 text-xl text-[#3B3C4A]">
           One of the most rewarding aspects of traveling is immersing yourself
           in the local culture and customs. This includes trying local cuisine,
           attending cultural events and festivals, and interacting with locals.
           Learning a few phrases in the local language can also go a long way in
           making connections and showing respect.
         </p>
-        <p className=" mb-8 text-xl">
+        <p className=" mb-8 text-xl text-[#3B3C4A]">
           One of the most rewarding aspects of traveling is immersing yourself
           in the local culture and customs. This includes trying local cuisine,
           attending cultural events and festivals, and interacting with locals.
           Learn ing a few phrases in the local language can also go a long way
           in making connections and showing respect.
         </p>
-        <p className=" mb-8 text-xl">
+        <p className=" mb-8 text-xl text-[#3B3C4A]">
           One of the most rewarding aspects of traveling is immersing yourself
           in the local culture and customs. This includes trying local cuisine,
           attending cultural events and festivals, and interacting with locals.
           Learning a few phrases in the local language can also go a long way in
           making connections and showing respect.
         </p>
-        <p className=" mb-8 text-xl">
+        <p className=" mb-8 text-xl text-[#3B3C4A]">
           One of the most rewarding aspects of traveling is immersing yourself
           in the local culture and customs. This includes trying local cuisine,
           attending cultural events and festivals, and interacting with locals.
