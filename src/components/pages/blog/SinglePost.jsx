ef28c34ff6e3014b167ecfadd55f7d42f17dcdb5
Joe Mayinzi <joemayinzi@gmail.com>
2023-09-17 10:55:02 +0100

    done theme mode single post

--- src/components/pages/blog/SinglePost.jsx
+++ src/components/pages/blog/SinglePost.jsx
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import AuthorPic from "../../../assets/img/Image.png";
 import SingleBlogImg from "../../../assets/img/singleblog.png";
 import { useParams } from "react-router-dom";
 import { useGetPosts } from "../../../utils/hooks/GetPosts";
 import { BASE_URL } from "../../../utils/api/BaseUrl";
+import { ThemeContext } from "../../../utils/context/ThemeContext";
 
 const SinglePost = () => {
   const { id } = useParams();
   const [singlePost] = useGetPosts(`${BASE_URL}/${id}`);
-  console.log(singlePost);
+  const [theme] = useContext(ThemeContext);
 
   return (
     <section>
       <div className="container mx-auto mt-20 w-full md:w-[804px] ">
-        <h2 className=" font-semibold mt-5 mb-5 text-3xl">
+        <h2
+          className={`font-semibold mt-5 mb-5 text-3xl ${
+            theme === "light" ? "" : "text-white"
+          }`}
+        >
           {singlePost.title}
         </h2>
         <div className="flex items-center gap-3 mb-9">
@@ -26,17 +31,84 @@ const SinglePost = () => {
           className="w-full mb-8"
           alt="single blog img"
         />
+        <p
+          className={`mb-8 text-xl ${
+            theme === "light" ? "text-[#3B3C4A]" : " text-white"
+          } "`}
+        >
+          {singlePost.body}
+        </p>
+        <p
+          className={`mb-8 text-xl ${
+            theme === "light" ? "text-[#3B3C4A]" : " text-white"
+          } "`}
+        >
+          {singlePost.body}
+        </p>
+        <p
+          className={`mb-8 text-xl ${
+            theme === "light" ? "text-[#3B3C4A]" : " text-white"
+          } "`}
+        >
+          {singlePost.body}
+        </p>
+        <p
+          className={`mb-8 text-xl ${
+            theme === "light" ? "text-[#3B3C4A]" : " text-white"
+          } "`}
+        >
+          {singlePost.body}
+        </p>
         <p className=" mb-8 text-xl text-[#3B3C4A] ">{singlePost.body}</p>
-        <p className=" mb-8 text-xl text-[#3B3C4A] ">{singlePost.body}</p>
-        <p className=" mb-8 text-xl text-[#3B3C4A] ">{singlePost.body}</p>
-        <p className=" mb-8 text-xl text-[#3B3C4A] ">{singlePost.body}</p>
-        <p className=" mb-8 text-xl text-[#3B3C4A] ">{singlePost.body}</p>
-        <p className=" mb-8 text-xl text-[#3B3C4A] ">{singlePost.body}</p>
-        <p className=" mb-8 text-xl text-[#3B3C4A] ">{singlePost.body}</p>
-        <p className=" mb-8 text-xl text-[#3B3C4A] ">{singlePost.body}</p>
-        <p className=" mb-8 text-xl text-[#3B3C4A] ">{singlePost.body}</p>
-        <p className=" mb-8 text-xl text-[#3B3C4A] ">{singlePost.body}</p>
-        <p className=" mb-8 text-xl text-[#3B3C4A] ">{singlePost.body}</p>
+        <p
+          className={`mb-8 text-xl ${
+            theme === "light" ? "text-[#3B3C4A]" : " text-white"
+          } "`}
+        >
+          {singlePost.body}
+        </p>
+        <p
+          className={`mb-8 text-xl ${
+            theme === "light" ? "text-[#3B3C4A]" : " text-white"
+          } "`}
+        >
+          {singlePost.body}
+        </p>
+        <p
+          className={`mb-8 text-xl ${
+            theme === "light" ? "text-[#3B3C4A]" : " text-white"
+          } "`}
+        >
+          {singlePost.body}
+        </p>
+        <p
+          className={`mb-8 text-xl ${
+            theme === "light" ? "text-[#3B3C4A]" : " text-white"
+          } "`}
+        >
+          {singlePost.body}
+        </p>
+        <p
+          className={`mb-8 text-xl ${
+            theme === "light" ? "text-[#3B3C4A]" : " text-white"
+          } "`}
+        >
+          {singlePost.body}
+        </p>
+        <p
+          className={`mb-8 text-xl ${
+            theme === "light" ? "text-[#3B3C4A]" : " text-white"
+          } "`}
+        >
+          {singlePost.body}
+        </p>
+        <p
+          className={`mb-8 text-xl ${
+            theme === "light" ? "text-[#3B3C4A]" : " text-white"
+          } "`}
+        >
+          {singlePost.body}
+        </p>
       </div>
     </section>
   );
