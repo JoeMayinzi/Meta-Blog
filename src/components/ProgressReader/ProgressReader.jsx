4271f26ec639ffdadbd2db1cc02fe34293a45283
Joe mayinzi <joemayinzi@gmail.com>
2024-02-26 14:32:33 +0100

    Add progressReader feature

--- src/components/ProgressReader/ProgressReader.jsx
+++ src/components/ProgressReader/ProgressReader.jsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from "react";
+
 const ProgressReader = () => {
+  const [scrollY, setSrcollY] = useState(0);
+  const lastValueScrolledY = document.body.offsetHeight - window.innerHeight;
+
+  useEffect(() => {
+    document.addEventListener("scroll", () => {
+      const scrollValue = window.scrollY;
+      setSrcollY(scrollValue);
+      console.log(scrollY);
+    });
+  }, [scrollY]);
+
   return (
     <div className="relative">
       <progress
         id="file"
-        max="100"
-        value="70"
+        max={lastValueScrolledY}
+        value={scrollY}
         className="w-full h-3 mb-3 fixed top-[76px]"
       >
         70%
